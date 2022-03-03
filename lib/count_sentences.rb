dc2f003d7a8d263f9a11bde14dc6a4475881b311
Zach <zawood11@gmail.com>
2022-03-03 00:36:38 -0500

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
 require 'pry'
 
 class String
-
+  attr_reader :count_sentences
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/[!?.] /)
+    sentences.count
+    #binding.pry
   end
 end
+
+"one. two? three!".count_sentences
